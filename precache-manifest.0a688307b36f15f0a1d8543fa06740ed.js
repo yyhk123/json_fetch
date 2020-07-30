7de6157ace7769a17adb71dd69f021198e772bfa
yyhk123 <minoook@hotmail.com>
2020-07-30 01:16:04 -0500

    Updates

---
+++ precache-manifest.0a688307b36f15f0a1d8543fa06740ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "325a6b9496b28fab634ce1fd67b2c2ea",
+    "url": "/json_fetch/index.html"
+  },
+  {
+    "revision": "199fbdb2ffce55cb0200",
+    "url": "/json_fetch/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c269bc20385ca8cf080f",
+    "url": "/json_fetch/static/js/2.109d8274.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/json_fetch/static/js/2.109d8274.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "199fbdb2ffce55cb0200",
+    "url": "/json_fetch/static/js/main.3c7e61fb.chunk.js"
+  },
+  {
+    "revision": "7d47aa7e6c653819c766",
+    "url": "/json_fetch/static/js/runtime-main.f9ecb393.js"
+  }
+]);
